33bee6411373579c089c69b63567348815839757
Grant Timmerman <timmerman+devrel@google.com>
2018-04-18 15:11:57 -0700

    Update sheets quickstart with final gitignore

--- sheets/SheetsQuickstart/Program.cs
+++ sheets/SheetsQuickstart/Program.cs
@@ -1,20 +1,4 @@
-/**
- * @license
- * Copyright Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-using Google.Apis.Auth.OAuth2;
+﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
@@ -58,10 +42,10 @@ namespace SheetsQuickstart
 
             // Create Google Sheets API service.
             var service = new SheetsService(new BaseClientService.Initializer()
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = ApplicationName,
-                });
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
 
             // Define request parameters.
             String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
@@ -87,6 +71,8 @@ namespace SheetsQuickstart
                 Console.WriteLine("No data found.");
             }
             Console.Read();
+
+
         }
     }
 }
